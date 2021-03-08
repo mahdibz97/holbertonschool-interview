107b3e4337f519cdc34193beedf6428b16b02725
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2021-03-08 23:59:40 +0100

    lock box

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""lockbox"""
+
+
+def canUnlockAll(boxes):
+    """can unlock all boxes"""
+    newlist = []
+    k = len(boxes)
+    for i in boxes:
+        if len(i) == 0 and i is not boxes[k-1]:
+            return False
+        for j in i:
+            newlist.append(j)
+    for index, keys in enumerate(boxes):
+        if index in newlist or index < k-1:
+            return True
+        else:
+            return False
